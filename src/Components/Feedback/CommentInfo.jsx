492950ecfa616ff55bfdc980e213903f728d5dcc
Biki-das <bikid475@gmail.com>
2023-06-07 09:30:39 +0530

    Changed comment icon color

--- src/Components/Feedback/CommentInfo.jsx
+++ src/Components/Feedback/CommentInfo.jsx
@@ -2,9 +2,9 @@ import { MessageCircle } from "react-feather";
 
 function CommentInfo({ comments }) {
   return (
-    <div className="flex items-end md:items-center">
-      <a className="flex items-center gap-x-2" href="/">
-        <MessageCircle stroke="#CDD3EF" />
+    <div className="flex items-end md:items-center ml-2">
+      <a className="flex items-center gap-x-1" href="/">
+        <MessageCircle fill="#CDD2EE" stroke="white" />
         <span className="font-bold">{comments.length}</span>
       </a>
     </div>
