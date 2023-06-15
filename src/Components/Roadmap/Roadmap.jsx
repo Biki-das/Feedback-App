0fe4b9cd8824f64775a53806fb08e7d40d642857
Biki-das <bikid475@gmail.com>
2023-06-15 22:53:37 +0530

    Added complete DND support

--- src/Components/Roadmap/Roadmap.jsx
+++ src/Components/Roadmap/Roadmap.jsx
@@ -1,13 +1,18 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { MdOutlineArrowBackIos } from "react-icons/md";
 import { AiOutlinePlus } from "react-icons/ai";
-import { supabase } from "../../Supabase/Supabaseconfig";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { supabase } from "../../Supabase/Supabaseconfig";
 
 function Roadmap() {
   const navigate = useNavigate();
   const [feedbackList, setFeedbackList] = useState(null);
+  const [columns, setColumns] = useState({
+    planned: [],
+    inProgress: [],
+    live: [],
+  });
 
   useEffect(() => {
     getFeedback();
@@ -16,8 +21,91 @@ function Roadmap() {
         .from("feedback")
         .select("*, upvotes(*),comments(*)");
       setFeedbackList(data);
+
+      const columnData = {
+        planned: data.filter((feedback) => feedback.status === "Planned"),
+        inProgress: data.filter((feedback) => feedback.status === "Inprogress"),
+        live: data.filter((feedback) => feedback.status === "Live"),
+      };
+      setColumns(columnData);
     }
   }, []);
+
+  const onDragEnd = async (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumnId = source.droppableId;
+      const destColumnId = destination.droppableId;
+      const sourceItems = [...columns[sourceColumnId]];
+      const destItems = [...columns[destColumnId]];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+
+      const updatedColumns = {
+        ...columns,
+        [sourceColumnId]: sourceItems,
+        [destColumnId]: destItems,
+      };
+
+      setColumns(updatedColumns);
+
+      const feedbackId = removed.id;
+      let feedbackStatus = "";
+      if (destColumnId === "planned") {
+        feedbackStatus = "Planned";
+      } else if (destColumnId === "inProgress") {
+        feedbackStatus = "Inprogress";
+      } else if (destColumnId === "live") {
+        feedbackStatus = "Live";
+      }
+
+      // Update feedback status directly in the columns state
+      updatedColumns[destColumnId] = updatedColumns[destColumnId].map(
+        (feedback) =>
+          feedback.id === feedbackId
+            ? { ...feedback, status: feedbackStatus }
+            : feedback
+      );
+      setColumns(updatedColumns);
+
+      // Update feedback status in the database
+      await updateFeedbackStatus(feedbackStatus, feedbackId);
+    } else {
+      const columnId = source.droppableId;
+      const copiedItems = [...columns[columnId]];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+
+      const updatedColumns = {
+        ...columns,
+        [columnId]: copiedItems,
+      };
+
+      setColumns(updatedColumns);
+    }
+  };
+
+  async function updateFeedbackStatus(feedbackStatus, feedbackId) {
+    const { error } = await supabase
+      .from("feedback")
+      .update({ status: feedbackStatus })
+      .eq("id", feedbackId);
+    if (error) {
+      console.log("Error updating feedback status:", error);
+    }
+  }
+
+  function getFeedbackCardstyles(feedbackStatus) {
+    if (feedbackStatus === "Planned") {
+      return "border-orange-400";
+    } else if (feedbackStatus === "Inprogress") {
+      return "border-purple-400";
+    } else {
+      return "border-cyan-400";
+    }
+  }
 
   return (
     <div className="w-[80%] mx-auto">
@@ -44,132 +132,65 @@ function Roadmap() {
           Add Feedback
         </button>
       </div>
-      <div>
-        <div className="mt-6 flex justify-between w-full">
-          <DragDropContext onDragEnd={(res) => console.log(res)}>
-            <Droppable droppableId="planned">
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    className="flex-1"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                  >
-                    {provided.placeholder}
-                    <PlannedFeedback feedbackList={feedbackList} />
-                  </div>
-                );
-              }}
-            </Droppable>
-            <Droppable droppableId="inProgress">
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    className="flex-1"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                  >
-                    {provided.placeholder}
-                    <InProgressFeedback feedbackList={feedbackList} />
-                  </div>
-                );
-              }}
-            </Droppable>
-            <Droppable droppableId="live">
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    className="flex-1"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                  >
-                    {provided.placeholder}
-                    <LiveFeedback feedbackList={feedbackList} />
-                  </div>
-                );
-              }}
-            </Droppable>
-          </DragDropContext>
-        </div>
+      <div className="mt-8">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="flex">
+            {Object.entries(columns).map(([columnId, column]) => {
+              return (
+                <div key={columnId} className="w-[33.33%]">
+                  <h3 className="text-lg font-bold text-center text-gray-600">
+                    {columnId === "planned"
+                      ? "Planned"
+                      : columnId === "inProgress"
+                      ? "In Progress"
+                      : "Live"}
+                  </h3>
+                  <Droppable droppableId={columnId}>
+                    {(provided) => (
+                      <ul
+                        className="mt-4 p-4  rounded-lg"
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                      >
+                        {column.map((feedback, index) => {
+                          return (
+                            <Draggable
+                              key={feedback.id}
+                              draggableId={feedback.id.toString()}
+                              index={index}
+                            >
+                              {(provided) => (
+                                <li
+                                  className={`bg-white rounded-lg p-4 mb-4 shadow-md border-t-8 ${getFeedbackCardstyles(
+                                    feedback.status
+                                  )}`}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  ref={provided.innerRef}
+                                >
+                                  <h4 className="font-bold">
+                                    {feedback.title}
+                                  </h4>
+                                  <p className="mt-2 text-gray-600">
+                                    {feedback.description}
+                                  </p>
+                                </li>
+                              )}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </ul>
+                    )}
+                  </Droppable>
+                </div>
+              );
+            })}
+          </div>
+        </DragDropContext>
       </div>
     </div>
   );
 }
 
 export default Roadmap;
-
-function PlannedFeedback({ feedbackList }) {
-  return feedbackList
-    ?.filter((feedback) => feedback.status === "Planned")
-    .map((plannedFeedback, index) => {
-      return (
-        <Draggable key={index} draggableId={`planned-${index}`} index={index}>
-          {(provided, snapshot) => {
-            return (
-              <Feedbackcard
-                title={plannedFeedback.title}
-                dragRef={provided.innerRef}
-                dragHandleProps={provided.dragHandleProps}
-                draggableProps={provided.draggableProps}
-              />
-            );
-          }}
-        </Draggable>
-      );
-    });
-}
-
-function InProgressFeedback({ feedbackList }) {
-  return feedbackList
-    ?.filter((feedback) => feedback.status === "Inprogress")
-    .map((inprogressFeedback, index) => {
-      return (
-        <Draggable key={index} draggableId={`inProgress-${index}`} index={index}>
-          {(provided, snapshot) => {
-            return (
-              <Feedbackcard
-                title={inprogressFeedback.title}
-                dragRef={provided.innerRef}
-                dragHandleProps={provided.dragHandleProps}
-                draggableProps={provided.draggableProps}
-              />
-            );
-          }}
-        </Draggable>
-      );
-    });
-}
-
-function LiveFeedback({ feedbackList }) {
-  return feedbackList
-    ?.filter((feedback) => feedback.status === "Live")
-    .map((liveFeedback, index) => {
-      return (
-        <Draggable key={index} draggableId={`live-${index}`} index={index}>
-          {(provided, snapshot) => {
-            return (
-              <Feedbackcard
-                title={liveFeedback.title}
-                dragRef={provided.innerRef}
-                dragHandleProps={provided.dragHandleProps}
-                draggableProps={provided.draggableProps}
-              />
-            );
-          }}
-        </Draggable>
-      );
-    });
-}
-
-function Feedbackcard({ dragRef, dragHandleProps, draggableProps,title }) {
-  return (
-    <div
-      ref={dragRef}
-      {...draggableProps}
-      {...dragHandleProps}
-      className="rounded-lg bg-white h-[300px] w-[90%] mx-auto border-t-8  border-orange-600 mt-6"
-    >
-      <h2>{title}</h2>
-    </div>
-  );
-}
