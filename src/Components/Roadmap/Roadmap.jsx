c8f6ff1a7e26fb934d69c21ed999ac2a3b1aebfc
Biki-das <bikid475@gmail.com>
2023-06-16 22:48:39 +0530

    Card redesign push

--- src/Components/Roadmap/Roadmap.jsx
+++ src/Components/Roadmap/Roadmap.jsx
@@ -13,6 +13,13 @@ import { Loader } from "../Utils/Loader";
 function Roadmap() {
   const navigate = useNavigate();
   const [feedbackList, setFeedbackList] = useState(null);
+  const isMobile = useMedia(["(max-width: 767px)"], [true], false);
+  const isTablet = useMedia(
+    ["(min-width: 768px)", "(max-width: 1023px)"],
+    [true, false],
+    false
+  );
+  const isDesktop = useMedia(["(min-width: 1024px)"], [true], false);
   const [loading, setLoading] = useState(false);
   const userId = useSelector((state) => {
     return state.currentUser?.user;
@@ -35,7 +42,6 @@ function Roadmap() {
       let { data } = await supabase
         .from("feedback")
         .select("*, upvotes(*),comments(*)");
-      console.log(data);
       setFeedbackList(data);
       setLoading(false);
 
@@ -51,6 +57,33 @@ function Roadmap() {
       toast.remove();
     };
   }, []);
+
+  function useMedia(queries, values, defaultValue) {
+    const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+
+    const getValue = () => {
+      const index = mediaQueryLists.findIndex((mql) => mql.matches);
+      return typeof values[index] !== "undefined"
+        ? values[index]
+        : defaultValue;
+    };
+
+    const [value, setValue] = useState(getValue);
+
+    useEffect(() => {
+      const handleResize = () => setValue(getValue);
+
+      // Add event listener for window resize
+      window.addEventListener("resize", handleResize);
+
+      // Clean up the event listener on unmount
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+
+    return value;
+  }
 
   const onDragEnd = async (result) => {
     if (!result.destination) return;
@@ -158,117 +191,127 @@ function Roadmap() {
           Add Feedback
         </button>
       </div>
-      <div className="mt-8">
-        {loading ? (
-          <Loader />
-        ) : (
-          <DragDropContext onDragEnd={onDragEnd}>
-            <div className="flex xl:w-[90%] mx-auto">
-              {Object.entries(columns).map(([columnId, column]) => {
-                return (
-                  <div key={columnId} className="w-[33.33%]">
-                    <h3 className="text-lg font-bold text-center text-gray-600">
-                      {columnId === "planned"
-                        ? `Planned (${columns.planned.length})`
-                        : columnId === "inProgress"
-                        ? `In Progress (${columns.inProgress.length})`
-                        : `Live (${columns.live.length})`}
-                    </h3>
-                    <Droppable droppableId={columnId}>
-                      {(provided) => (
-                        <ul
-                          className="mt-4 p-4  rounded-lg"
-                          {...provided.droppableProps}
-                          ref={provided.innerRef}
-                        >
-                          {column.map((feedback, index) => {
-                            return (
-                              <Draggable
-                                key={feedback.id}
-                                draggableId={feedback.id.toString()}
-                                index={index}
-                              >
-                                {(provided) => (
-                                  <li
-                                    className={`bg-white w-full  mx-auto rounded-lg p-4 mb-4 shadow-md border-t-8 ${getFeedbackCardstyles(
-                                      feedback.status
-                                    )}`}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    ref={provided.innerRef}
-                                  >
-                                    <h4 className="font-bold">
-                                      {feedback.title}
-                                    </h4>
-                                    <p className="mt-2 text-gray-600">
-                                      {feedback.description}
-                                    </p>
-                                    <div className="bg-blue-50 rounded-md py-1 px-4 h-[25px] w-fit flex items-center mt-2">
-                                      <p className="text-sm text-blue-600 font-medium">
-                                        {feedback.category}
+      {isTablet && (
+        <div className="mt-8">
+          {loading ? (
+            <Loader />
+          ) : (
+            <DragDropContext onDragEnd={onDragEnd}>
+              <div className="flex xl:w-[90%] mx-auto">
+                {Object.entries(columns).map(([columnId, column]) => {
+                  return (
+                    <div key={columnId} className="w-[33.33%]">
+                      <h3 className="text-lg font-bold text-center text-gray-600">
+                        {columnId === "planned"
+                          ? `Planned (${columns.planned.length})`
+                          : columnId === "inProgress"
+                          ? `In Progress (${columns.inProgress.length})`
+                          : `Live (${columns.live.length})`}
+                      </h3>
+                      <Droppable droppableId={columnId}>
+                        {(provided) => (
+                          <ul
+                            className="mt-4 p-4  rounded-lg"
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                          >
+                            {column.map((feedback, index) => {
+                              return (
+                                <Draggable
+                                  key={feedback.id}
+                                  draggableId={feedback.id.toString()}
+                                  index={index}
+                                >
+                                  {(provided) => (
+                                    <li
+                                      className={`bg-white w-full min-h-[200px] relative mx-auto rounded-lg p-6 mb-4 shadow-md border-t-8 ${getFeedbackCardstyles(
+                                        feedback.status
+                                      )}`}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      ref={provided.innerRef}
+                                    >
+                                      <h4 className="font-bold">
+                                        {feedback.title}
+                                      </h4>
+                                      <p className="mt-2 text-gray-600">
+                                        {isDesktop
+                                          ? feedback.description
+                                          : feedback.description.substring(
+                                              0,
+                                              80
+                                            ) + "......"}
                                       </p>
-                                    </div>
-                                    <div className="flex justify-between items-center">
-                                      <CommentInfo
-                                        comments={feedback.comments}
-                                      />
-                                      <UpvoteButtonContainer
-                                        feedbackId={feedback.id}
-                                        userId={userId}
-                                        upvotes={feedback.upvotes}
-                                      />
-                                    </div>
-                                    <div class="flex mb-5 -space-x-4 items-center">
-                                      {feedback.comments.map(
-                                        ({ authorAvatar }, index) => {
-                                          if (
-                                            feedback.comments.length > 2 &&
-                                            index === 1
-                                          ) {
-                                            const remainingCount =
-                                              feedback.comments.length - 2;
-                                            return (
-                                              <>
+
+                                      <div className="flex flex-row-reverse justify-between items-center mt-2">
+                                        <CommentInfo
+                                          comments={feedback.comments}
+                                        />
+                                        <div className="bg-blue-50 rounded-md py-1 px-4 h-[25px] w-fit flex items-center">
+                                          <p className="text-sm text-blue-600 font-medium">
+                                            {feedback.category}
+                                          </p>
+                                        </div>
+                                      </div>
+                                      <div className="relative top-4">
+                                        <UpvoteButtonContainer
+                                          feedbackId={feedback.id}
+                                          userId={userId}
+                                          upvotes={feedback.upvotes}
+                                        />
+                                      </div>
+                                      <div class="flex bottom-2 -space-x-4 items-center absolute right-2">
+                                        {feedback.comments.map(
+                                          ({ authorAvatar }, index) => {
+                                            if (
+                                              feedback.comments.length > 2 &&
+                                              index === 1
+                                            ) {
+                                              const remainingCount =
+                                                feedback.comments.length - 2;
+                                              return (
+                                                <>
+                                                  <img
+                                                    class={`${"w-[35px] h-[35px]"}  border-2  rounded-full ${getFeedbackCardstyles(
+                                                      feedback.status
+                                                    )}`}
+                                                    src={authorAvatar}
+                                                    key={index}
+                                                  />
+                                                  <span class="flex items-center justify-center w-[35px] h-[35px] text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800">{`+${remainingCount}`}</span>
+                                                </>
+                                              );
+                                            } else if (index < 2) {
+                                              return (
                                                 <img
-                                                  class={`w-10 h-10 border-2  rounded-full ${getFeedbackCardstyles(
+                                                  class={`${"w-[35px] h-[35px]"}  border-2  rounded-full ${getFeedbackCardstyles(
                                                     feedback.status
                                                   )}`}
                                                   src={authorAvatar}
                                                   key={index}
                                                 />
-                                                <span class="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800">{`+${remainingCount}`}</span>
-                                              </>
-                                            );
-                                          } else if (index < 2) {
-                                            return (
-                                              <img
-                                                class={`w-10 h-10 border-2 rounded-full ${getFeedbackCardstyles(
-                                                  feedback.status
-                                                )}`}
-                                                src={authorAvatar}
-                                                key={index}
-                                              />
-                                            );
+                                              );
+                                            }
                                           }
-                                        }
-                                      )}
-                                    </div>
-                                  </li>
-                                )}
-                              </Draggable>
-                            );
-                          })}
-                          {provided.placeholder}
-                        </ul>
-                      )}
-                    </Droppable>
-                  </div>
-                );
-              })}
-            </div>
-          </DragDropContext>
-        )}
-      </div>
+                                        )}
+                                      </div>
+                                    </li>
+                                  )}
+                                </Draggable>
+                              );
+                            })}
+                            {provided.placeholder}
+                          </ul>
+                        )}
+                      </Droppable>
+                    </div>
+                  );
+                })}
+              </div>
+            </DragDropContext>
+          )}
+        </div>
+      )}
     </div>
   );
 }
