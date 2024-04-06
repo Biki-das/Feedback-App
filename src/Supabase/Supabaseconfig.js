4da1478db2cdacb1e8031f3291710f75dee2a2b5
Biki-das <bikid475@gmail.com>
2024-04-06 22:53:52 +0530

    typescript init and redux folder migrated  to ts

--- src/Supabase/Supabaseconfig.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
-const supabaseToken =
-  import.meta.env.VITE_SUPABASE_TOKEN || process.env.SUPABASE_TOKEN;
-
-export const supabase = createClient(supabaseUrl, supabaseToken);
